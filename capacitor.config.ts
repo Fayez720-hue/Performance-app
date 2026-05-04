8b1376c4e5b5fb9b929ed21f61df0c2c8142445d
Original <ahmedfayez720@gmail.com>
2026-05-04 17:13:09 +0300

    signin loop fix

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.canshift.performanceapp',
   appName: 'Performance App',
-  webDir: 'out',                         // must match your Next.js export directory
+  webDir: 'out',                         // static export folder
   android: {
     allowMixedContent: true,
     overrideUserAgent: 'Mozilla/5.0 (Linux; Android 13; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Mobile Safari/537.36'
